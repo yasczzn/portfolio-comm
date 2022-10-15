37e15d44fa4d7f189f7c4d886101cebe308763ff
yaswu <putriyasminarahmaz@gmail.com>
2022-10-15 22:11:12 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Background from './bg.png';
+
+var sectionStyle = {
+  width: "100%",
+  height: "400px",
+  backgroundImage: "url(" + { Background } + ")"
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content" style={ sectionStyle }>
+        <p>Hello _World</p>
+      </div>
     </div>
   );
 }
