9eea339c166766acb50f5e65db5846f5e5d4b002
yaswu <putriyasminarahmaz@gmail.com>
2022-10-15 22:25:01 +0700

    second commit
    edit 1 file, add 1 file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Background from './bg.png';
+import Footer from './Footer'
 
 var sectionStyle = {
   width: "100%",
@@ -12,6 +13,7 @@ function App() {
       <div className="content" style={ sectionStyle }>
         <p>Hello _World</p>
       </div>
+      <Footer/>
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return ( 
+    <footer className="footer">
+        <footer class="bg-light py-5">
+            <div class="container px-4 px-lg-5"><div class="small text-center text-muted">Copyright &copy; 2022 - yasczzn</div></div>
+        </footer>
+    </footer> 
+    );
+}
+ 
+export default Footer;
